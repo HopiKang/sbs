a018161e4e5554ef09062a7435f535353492d80e
HopiKang <110038354+HopiKang@users.noreply.github.com>
2023-01-19 21:59:17 +0900

    Day52
    java

---
+++ 5_algorithm/Decryption.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/*
+3. 로미오는 원수의 딸인 줄리엣에게 알파벳 대문자로 구성된 비밀편지를 email로 매일 보낸다
+비밀편지는 로미오와 줄리엣만 알고잇는 암호로 만들어 졌습니다 비밀편지는 알파벳 한 문자마다
+#또는 *가 일곱개씩 구성되어 있습니다 만약 로미오가 #*****#으로 구성된 문자를 보냈다면
+줄리엣은 로미오와 약속한 규칙대로 다음과 같이 해석 할 수 있습니다
+- #*****# >> 1000001 일곱자리의 이진수로 바꿉니다. #은 이진수 1로, *은 이진수 0으로 변환
+- 아스키 번호가 65문자로 변환되면 65는 대분자 A입니다 ex) A : 65 , B : 66 ~ Z : 90
+로미호가 4개의 문자를 아래와 같이 신호를 보냈다면
+##*##**
+##*####
+###*##*
+##**#*#
+출력되는 문자는 무엇인가요
+*/
+
+public class Decryption {
+    /*
+    public static void main(String[] args) {
+        Decryption decryption = new Decryption();
+        Scanner keyBoardInput = new Scanner(System.in);
+        String str = keyBoardInput.next();
+        System.out.println(decryption.resolve(str));
+    }
+
+    public String resolve(String s){
+
+    }
+
+
+ */
+}
